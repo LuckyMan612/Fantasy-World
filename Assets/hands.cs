3dd59ec10da9d67b0574db5af0768e4f8bfc1ca8
LuckyMan612 <75744132+LuckyMan612@users.noreply.github.com>
2023-07-27 01:18:15 +0200

    Animacje

--- Assets/hands.cs
+++ Assets/hands.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class hands : MonoBehaviour
 {
     public FirstPersonController fpc;
+    public GameObject rightHand;
+    public GameObject leftHand;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,21 @@ public class hands : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(fpc.isSprinting);
+        if (fpc.isWalking)
+        {
+            leftHand.GetComponent<Animator>().enabled = true;
+            rightHand.GetComponent<Animator>().enabled = true;
+        }
+        else if (fpc.isSprinting)
+        {
+            leftHand.GetComponent<Animator>().speed = 1.5f;
+            rightHand.GetComponent<Animator>().speed = 1.5f;
+        }
+        else
+        {
+            leftHand.GetComponent<Animator>().enabled = false;
+            rightHand.GetComponent<Animator>().enabled = false;
+        }
+
     }
 }
