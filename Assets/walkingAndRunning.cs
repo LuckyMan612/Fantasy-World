fdd6443cfc532a50101d512ea360efdc1d2ccf70
LuckyMan612 <75744132+LuckyMan612@users.noreply.github.com>
2023-07-27 19:12:54 +0200

    Pozmienioano troche

--- Assets/walkingAndRunning.cs
+++ Assets/walkingAndRunning.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class walkingAndRunning : MonoBehaviour
 {
+    public FirstPersonController fpc;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,21 @@ public class walkingAndRunning : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!fpc.isWalking)
+        {
+            GetComponent<Animator>().enabled = false;
+        }
+        else if (fpc.isWalking)
+        {
+            GetComponent<Animator>().enabled = true;
+        }
+        if (fpc.isSprinting)
+        {
+            GetComponent<Animator>().speed = 0.5f;
+        }
+        else if (!fpc.isSprinting)
+        {
+            GetComponent<Animator>().speed = 0.3f;
+        }
     }
 }
